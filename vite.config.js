4044149e39fe98b24fb6cf0c2b915283f09d7a43
eunsunpark99 <eunsunpark99@gmail.com>
2026-06-16 20:05:22 +0900

    fix vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/gallery-project/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
