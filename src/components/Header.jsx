38b505bbceedfcd6dac775e291bd753aae981469
Eunsun Park <eunsunpark99-svg@users.noreply.github.com>
2026-06-19 01:05:13 +0900

    Refine header layout and social icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,9 +24,11 @@ const SocialIcon = ({ type }) => {
   if (type === 'instagram') {
     return (
       <svg aria-hidden="true" viewBox="0 0 24 24">
-        <rect x="5" y="5" width="14" height="14" rx="4" />
-        <circle cx="12" cy="12" r="3.2" />
-        <circle cx="16.2" cy="7.8" r="1" />
+        <path
+          fillRule="evenodd"
+          d="M5 4h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1Zm2.2 2.2v11.6h9.6V6.2H7.2Zm4.8 2.45a3.35 3.35 0 1 0 0 6.7 3.35 3.35 0 0 0 0-6.7Zm0 1.9a1.45 1.45 0 1 1 0 2.9 1.45 1.45 0 0 1 0-2.9Zm3.9-2.2a1.05 1.05 0 1 0 0 2.1 1.05 1.05 0 0 0 0-2.1Z"
+          clipRule="evenodd"
+        />
       </svg>
     )
   }
@@ -34,15 +36,22 @@ const SocialIcon = ({ type }) => {
   if (type === 'facebook') {
     return (
       <svg aria-hidden="true" viewBox="0 0 24 24">
-        <path d="M14.1 8.3h2V5.1c-.9-.1-1.8-.2-2.7-.2-2.7 0-4.5 1.6-4.5 4.6v2.6H6v3.6h2.9V23h3.8v-7.3h3l.5-3.6h-3.5V9.9c0-1 .3-1.6 1.4-1.6Z" />
+        <path
+          fillRule="evenodd"
+          d="M5 4h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1Zm8.1 15v-5.25h1.75l.28-2.05H13.1v-1.32c0-.6.18-1 1.05-1h1.08V7.55a14.5 14.5 0 0 0-1.62-.08c-1.6 0-2.7.98-2.7 2.78v1.45H9.1v2.05h1.8V19h2.2Z"
+          clipRule="evenodd"
+        />
       </svg>
     )
   }
 
   return (
     <svg aria-hidden="true" viewBox="0 0 24 24">
-      <path d="M4.5 5.5h15v10.6h-8.1L7.6 20v-3.9H4.5V5.5Z" />
-      <path d="M8.2 9h2.7c1.3 0 2.1.7 2.1 1.7 0 .6-.3 1.1-.8 1.4.7.3 1.1.8 1.1 1.6 0 1.2-.9 2-2.3 2H8.2V9Zm2.5 2.4c.5 0 .8-.2.8-.6s-.3-.6-.8-.6H9.8v1.2h.9Zm.2 3c.5 0 .8-.3.8-.7 0-.4-.3-.7-.9-.7h-1v1.4h1.1Z" />
+      <path
+        fillRule="evenodd"
+        d="M5 4h14a1 1 0 0 1 1 1v10.4a1 1 0 0 1-1 1h-5.8L9.3 20v-3.6H5a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1Zm2.2 2.2v8h4.1v1.35l1.25-1.35h5.25v-8H7.2Zm3.05 2.05h2.05c1.35 0 2.18.7 2.18 1.78 0 .56-.24 1.02-.7 1.34.62.3.96.8.96 1.5 0 1.2-.9 1.95-2.35 1.95h-2.14V8.25Zm1.5 2.35h.45c.48 0 .76-.2.76-.56s-.27-.55-.75-.55h-.46v1.11Zm0 2.98h.56c.52 0 .84-.25.84-.66 0-.42-.32-.66-.86-.66h-.54v1.32Z"
+        clipRule="evenodd"
+      />
     </svg>
   )
 }
@@ -116,8 +125,11 @@ export default function Header({
   return (
     <header className="site-header">
       <a className="brand" href="/" onClick={(event) => followLink(event, '/')}>
-        <span className="brand-mark">P</span>
-        <span>{siteContent.brandName}</span>
+        <span className="brand-mark" aria-hidden="true">E</span>
+        <span className="brand-name">
+          <span className="brand-name-ko">{siteContent.brandName}</span>
+          <span className="brand-name-en">Eunsun Park</span>
+        </span>
       </a>
 
       <button
